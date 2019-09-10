1bc56bb1eae5a6f6f8d9a33d2879091c2d85c3bd
Eli Sacks <bastlifa@gmail.com>
2019-09-10 15:27:06 -0400

    update, delete added

--- api/carsRouter.js
+++ api/carsRouter.js
@@ -58,4 +58,52 @@ router.post('/', (req, res) =>
     }
 })
 
+router.put('/:id', (req, res) =>
+{
+    const id = req.params.id
+    const changes = req.body
+    db('cars')
+        .where({id})
+        .update(changes)
+            .then(response =>
+                {
+                    if(response)
+                    {
+                        res.status(200).json({ message: `updated ${response} accounts` })
+                    }
+                    else
+                    {
+                        res.status(404).json({ errorMessage: `Could not update car with id ${id}`})
+                    }
+                })
+            .catch(error =>
+                {
+                    res.status(500).json({ errorMessage: "Internal Error: Could not update car" })
+                })
+})
+
+router.delete('/:id', (req, res) =>
+{
+    const id = req.params.id
+    const changes = req.body
+    db('cars')
+        .where({id})
+        .del()
+            .then(response =>
+                {
+                    if(response)
+                    {
+                        res.status(200).json({ message: `deleted ${response} accounts` })
+                    }
+                    else
+                    {
+                        res.status(404).json({ errorMessage: `Could not delete car with id ${id}`})
+                    }
+                })
+            .catch(error =>
+                {
+                    res.status(500).json({ errorMessage: "Internal Error: Could not delete car" })
+                })
+})
+
 module.exports = router
